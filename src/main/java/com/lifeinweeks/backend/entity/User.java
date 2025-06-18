0c9a842616d2e954df4f8478bdbe0e910f42bd33
P Sinha <95397232+PS2207@users.noreply.github.com>
2025-06-18 16:59:02 +0530

    Update User.java

--- src/main/java/com/lifeinweeks/backend/entity/User.java
+++ src/main/java/com/lifeinweeks/backend/entity/User.java
@@ -10,6 +10,7 @@ import java.time.LocalDate;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Table(name = "postgresql_user")
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
